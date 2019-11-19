67d43df67d842b079eea195760e5de86e2d4579d
Razak, Zaheir <zrazak3@gatech.edu>
2019-11-19 13:01:04 -0500

    added files to allow to see movies, and created view model and separated navigation bar to partial layout

---
+++ movies/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using movies.Models;
+using movies.ViewModels;
+using movies.ViewModels;
+
+namespace movies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Shrek" };
+
+
+            var customer = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customer
+            };
+
+            return View(viewModel);
+        }
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
